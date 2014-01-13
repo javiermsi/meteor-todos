1bb38b3d94da228970a72372aa3a1929c8446425
javiermsi <javiermsi@gmail.com>
2014-01-13 19:32:54 +0100

    Initial commit

--- meteoric.config.sh
+++ meteoric.config.sh
@@ -1,26 +1,26 @@
 # IP or URL of the server you want to deploy to
-APP_HOST=meteor.julien-c.fr
+APP_HOST=54.194.184.146
 
 # If you want a different ROOT_URL, when using a load balancer for instance, set it here
 #ROOT_URL=mycustom.url.com
 
 # Comment this if your host is not an EC2 instance
-EC2_PEM_FILE=~/.ssh/proxynet.pem
+EC2_PEM_FILE=~/Downloads/code/aws/aws-ireland-cv.pem
 
 # What's your project's Git repo?
-GIT_URL=git://github.com/SachaG/Microscope.git
+#GIT_URL=git://github.com/SachaG/Microscope.git
 
 # Does your project use meteorite, or plain meteor?
 METEORITE=true
 
 # If not using Meteorite, you need to specify this
-METEOR_RELEASE=0.6.4
+#METEOR_RELEASE=0.6.4
 
 #If you have an external service, such as Google SMTP, set this
 #MAIL_URL=smtp://USERNAME:PASSWORD@smtp.googlemail.com:465
 
 # What's your app name?
-APP_NAME=microscope
+APP_NAME=todos
 
 # If your app is not on the repository root, set this
 APP_PATH=.
@@ -30,9 +30,6 @@ GIT_BRANCH=master
 
 # Kill the forever and node processes, and deletes the bundle directory and tar file prior to deploying
 FORCE_CLEAN=false
-
-# Setup a listening port for your instance / default is 80
-#PORT=8000
 
 # If you want to do something before forever starts Meteor, you can do it here
 # NOTE: Don't forget to use a semi-colon at the end of every command
